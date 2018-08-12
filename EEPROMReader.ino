69d2bc4686b115a301761ca251ebdffcbceb87c5
Filmote <simon@bloggingadeadhorse.com>
2018-08-12 20:16:45 +1000

    Add update capability.

--- EEPROMReader.ino
+++ EEPROMReader.ino
@@ -4,11 +4,22 @@
 
 Arduboy2Base arduboy;
 Sprites sprite;
-
 Font4x6 font4x6 = Font4x6(0);
+
+enum class EditMode : uint8_t {
+
+  None,
+  Edit
+
+};
+
 char text[24];
+bool flash = false;
+uint8_t origValue;
+EditMode mode = EditMode::None;
 
 int16_t eepromLocation = EEPROM_STORAGE_SPACE_START;
+
 
 //------------------------------------------------------------------------------
 //  Setup 
@@ -25,6 +36,11 @@ void setup() {
 
 }
 
+
+//------------------------------------------------------------------------------
+//  Loop away!
+//------------------------------------------------------------------------------
+
 void loop() {
   
   if (!(arduboy.nextFrame())) return;
@@ -39,12 +55,13 @@ void loop() {
   arduboy.drawLine(71, 45, 79, 61, WHITE);
 
   font4x6.setCursor(2, 0);
-  font4x6.print("Location ");
+  font4x6.print(F("Location "));
   font4x6.print(eepromLocation);
 
   for (int16_t x = eepromLocation - 12, y = 0; x < eepromLocation + 13; x++, y++) {
 
-    uint8_t val = EEPROM.read((x < 0 ? x + 1024 : (x > 1023 ? x - 1024 : x)));
+    uint16_t location = (x < 0 ? x + 1024 : (x > 1023 ? x - 1024 : x));
+    uint8_t val = EEPROM.read(location);
 
     switch (y / 5) {
 
@@ -56,8 +73,12 @@ void loop() {
 
         if ((y % 5) == 2) {
 
-          arduboy.fillRect(((y % 5) * 15), ((y / 5) * 8) + 19, 13, 8, WHITE);
-          font4x6.setTextColor(BLACK);
+          if (mode == EditMode::None || flash) {
+
+            arduboy.fillRect(((y % 5) * 15), ((y / 5) * 8) + 19, 13, 8, WHITE);
+            font4x6.setTextColor(BLACK);
+
+          }
 
         }
         else {
@@ -75,9 +96,8 @@ void loop() {
 
     }
 
-    if (val < 16) { font4x6.print("0"); }
-    font4x6.print(val, HEX);
-
+    printDec(val);
+    
   }
 
   font4x6.setCursor(83, 15);
@@ -98,14 +118,46 @@ void loop() {
   printBinary(static_cast<uint16_t>(eepromLocation + 2));
 
 
-  if (arduboy.justPressed(LEFT_BUTTON))   --eepromLocation; if (eepromLocation < 0) eepromLocation = 1023;
-  if (arduboy.justPressed(RIGHT_BUTTON))  ++eepromLocation; if (eepromLocation > 1023) eepromLocation = 0;
-  if (arduboy.justPressed(UP_BUTTON))     eepromLocation = eepromLocation - 5; if (eepromLocation < 0) eepromLocation = eepromLocation + 1024;
-  if (arduboy.justPressed(DOWN_BUTTON))   eepromLocation = eepromLocation + 5; if (eepromLocation > 1023) eepromLocation = eepromLocation - 1024;
+  switch (mode) {
 
+    case EditMode::None:
+      if (arduboy.justPressed(LEFT_BUTTON))   { --eepromLocation; if (eepromLocation < 0) eepromLocation = 1023; }
+      if (arduboy.justPressed(RIGHT_BUTTON))  { ++eepromLocation; if (eepromLocation > 1023) eepromLocation = 0; }
+      if (arduboy.justPressed(UP_BUTTON))     { eepromLocation = eepromLocation - 5; if (eepromLocation < 0) eepromLocation = eepromLocation + 1024; }
+      if (arduboy.justPressed(DOWN_BUTTON))   { eepromLocation = eepromLocation + 5; if (eepromLocation > 1023) eepromLocation = eepromLocation - 1024; }
+
+      if (arduboy.justPressed(A_BUTTON))      { 
+        mode = EditMode::Edit; 
+        origValue = EEPROM.read(eepromLocation); 
+      }
+
+      break;
+
+    case EditMode::Edit:
+
+      font4x6.setCursor(93, 0);
+      font4x6.print(F("Orig "));
+      printDec(origValue);
+
+      if (arduboy.justPressed(UP_BUTTON))     incValue();
+      if (arduboy.justPressed(DOWN_BUTTON))   decValue();
+      if (arduboy.justPressed(A_BUTTON))      mode = EditMode::None;
+      break;
+
+  }
+
+  if (arduboy.everyXFrames(32)) { flash = !flash; }
   arduboy.display();
 
 }
+
+void printDec(uint8_t data) {
+
+  if (data < 16) { font4x6.print(F("0")); }
+  font4x6.print(data, HEX);
+
+}
+
 
 void printBinary(uint16_t location) {
 
@@ -124,4 +176,18 @@ void printBinary(uint16_t location) {
 
   font4x6.print(text);
 
+}
+
+void incValue() {
+
+  uint8_t data = EEPROM.read(eepromLocation);
+  EEPROM.update(eepromLocation, data + 1);
+
+}
+
+void decValue() {
+
+  uint8_t data = EEPROM.read(eepromLocation);
+  EEPROM.update(eepromLocation, data - 1);
+  
 }
